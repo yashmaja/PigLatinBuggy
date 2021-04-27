5f6a534eaa0ed0b9f5bb44317a32ef61c96fd0d4
Tommy Waalkes <Tommy@grandcircus.co>
2021-04-27 13:45:49 -0400

    Initial Commit, code is def perfect so I didn't test it

---
+++ PigLatin/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace PigLatin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
+
+            string translation = ToPigLatin(userInput);
+            Console.WriteLine(translation);
+        }
+
+        public static string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine().ToLower().Trim();
+            return input;
+        }
+
+        public static bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            
+            return c.ToString() == vowels.ToString();
+        }
+
+        public static string ToPigLatin(string word)
+        {
+            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+            word = word.ToLower();
+            foreach(char c in specialChars)
+            {
+                foreach(char w in word)
+                {
+                    if (w == c)
+                    {
+                        Console.WriteLine("That word has special characters, we will return it as is");
+                        return word;
+                    }
+                }
+                
+            }
+
+            bool noVowels = true;
+            foreach(char letter in word)
+            {
+                if (IsVowel(letter))
+                {
+                    noVowels = false;
+                }
+            }
+
+            if (noVowels)
+            {
+                return word; 
+            }
+
+            char firstLetter = word[0];
+            string output = "placeholder";
+            if (IsVowel(firstLetter) == true)
+            {
+                output = word + "ay";
+            }
+            else
+            {
+                int vowelIndex = -1;
+                //Handle going through all the consonants
+                for (int i = 0; i <= word.Length; i++)
+                {
+                    if (IsVowel(word[i]) == true)
+                    {
+                        vowelIndex = i;
+                        break;
+                    }
+                }
+
+                string sub = word.Substring(vowelIndex + 1);
+                string postFix = word.Substring(0, vowelIndex -1);
+
+                output = sub + postFix + "ay";
+            }
+
+            return output;
+        }
+    }
+}
